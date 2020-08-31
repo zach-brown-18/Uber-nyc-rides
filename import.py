2df80f869d1a8d068c1459c126f7eca27ca11d89
wangye <coconut@wangyedeAir.attlocal.net>
2020-08-30 18:09:22 -0700

    Cleaned import.py file

--- import.py
+++ import.py
@@ -10,27 +10,21 @@ df_july = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/uber-tl
 df_august = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-aug14.csv')
 df_september = pd.read_csv('https://raw.githubusercontent.com/fivethirtyeight/uber-tlc-foil-response/master/uber-trip-data/uber-raw-data-sep14.csv')
 
+frames = [df_april, df_may, df_june, df_july, df_august, df_september]
+
 # Add 'Hour' bin
 def to_hour(dateTimeStr:str)->str:
     return int(dateTimeStr.split(" ")[1].split(":")[0]) + 1
 
-frames = [df_april, df_may, df_june, df_july, df_august, df_september]
-
 for month in frames:  
     month["Hour"] = month["Date/Time"].apply(to_hour)
 
-df_april = df_april[["Date/Time", "Lat", "Lon", "Hour"]]
-df_may = df_may[["Date/Time", "Lat", "Lon", "Hour"]]
-df_june = df_june[["Date/Time", "Lat", "Lon", "Hour"]]
-df_july = df_july[["Date/Time", "Lat", "Lon", "Hour"]]
-df_august = df_august[["Date/Time", "Lat", "Lon", "Hour"]]
-df_september = df_september[["Date/Time", "Lat", "Lon", "Hour"]]
+column_selection = ["Date/Time", "Lat", "Lon", "Hour"]
+frames = [frame[column_selection] for frame in frames]
 
 def get_df_name(df):
     name =[x for x in globals() if globals()[x] is df][0]
     return name+'.csv'
-
-frames = [df_april, df_may, df_june, df_july, df_august, df_september]
 
 for month in frames:
     month.to_csv(get_df_name(month))
